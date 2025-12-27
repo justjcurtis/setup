e77f0424a975fd5506ec2c16a0130dc23f4c7708
justjcurtis <justjcurtis@gmail.com>
2025-12-27 23:01:27 +0100

    feat: ansi constants + prompt funcs

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,17 +1,96 @@
 #!/usr/bin/env bash
-set -e
 
-echo "→ justjcurtis.dev setup bootstrap"
-echo "→ https://github.com/justjcurtis/setup"
-echo
+RESET="\033[0m"
+BOLD="\033[1m"
+ITALIC="\033[3m"
+UNDERLINE="\033[4m"
+RED="\033[31m"
+BLUE="\033[34m"
+GREEN="\033[32m"
+YELLOW="\033[33m"
+CYAN="\033[36m"
+PURPLE="\033[35m"
+PINK="\033[95m"
+
+p() {
+    printf "$1${RESET}\n" 
+}
+
+select_option() {
+    local -n _opts=$1
+    local question=$2
+    local choice
+
+    p "${ITALIC}$question" 
+    for i in "${!_opts[@]}"; do
+        printf "${BOLD}${ITALIC}%d)${RESET} %s\n" $((i + 1)) "${_opts[i]}"
+    done
+
+    while true; do
+        read -rp "Enter choice [1-${#_opts[@]}]: " choice
+
+        if [[ "$choice" =~ ^[0-9]+$ ]] && (( choice >= 1 && choice <= ${#_opts[@]} )); then
+            return $((choice - 1))
+        else
+            printf "Invalid selection. Try again.\n"
+        fi
+    done
+}
+
+yes_no() {
+    local question=$1
+    local default_answer=$2
+    local response
+
+    local brackets="[y/n]"
+    if [[ "$default_answer" == "y" ]]; then
+        brackets="[Y/n]"
+    elif [[ "$default_answer" == "n" ]]; then
+        brackets="[y/N]"
+    fi
+
+    while true; do
+        read -rp "$question $brackets: " response
+        case "$response" in
+            [Yy]* ) return 0 ;;
+            [Nn]* ) return 1 ;;
+            * )
+                if [[ -n "$default_answer" ]]; then
+                    if [[ "$default_answer" == "y" ]]; then
+                        return 0
+                    else
+                        return 1
+                    fi
+                else
+                    printf "Please answer yes or no.\n"
+                fi
+                ;;
+        esac
+    done
+}
+
+
+p "→ ${BOLD}${RED}justjcurtis.dev${RESET}${BOLD} setup bootstrap"
+p "→ ${PURPLE}https://github.com/justjcurtis/setup"
+
 
 TMP="$(mktemp -d)"
 cd "$TMP"
 
-echo "→ Made temporary directory at $TMP"
-echo "→ Here I would clone the setup repository and run its install script"
+p "→ Made temporary directory at $TMP"
 
-echo "→ Cleaning up..."
-rm -rf "$TMP"
-echo "→ Removed temporary directory"
+options=("Burger" "Kebab" "Pizza")
+select_option options "Pick a meal:"
+selected_index=$?
+
+ending=""
+if yes_no "Would you like to add fries?" "y"; then
+    ending=" with fries"
+fi
+
+p "You selected value: ${options[selected_index]}${ending}"
+
+p "→ Cleaning up..."
+rm -rf "$TMP\n"
+p "→ Removed temporary directory"
 
